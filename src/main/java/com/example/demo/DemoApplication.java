d75f03aac4b59a304e1e733d4aa3188bd73416d4
Nikhil Murthy <47204237+nik-murthy@users.noreply.github.com>
2021-09-03 15:22:58 +1000

    02 checkstyle (#1)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Sample Javadoc.
+ */
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+  
 }
--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+/**
+ * Sample Javadoc.
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
-	}
-
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(DemoApplication.class);
+  }
 }
